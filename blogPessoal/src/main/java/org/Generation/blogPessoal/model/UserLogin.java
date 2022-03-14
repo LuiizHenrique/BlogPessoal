9065f61d009dbe7ac479655c78ee479137b4b044
Luiz Felipe Henrique Pereira <lfhenrique95@gmail.com>
2022-03-14 18:31:29 -0300

    ...

--- blogPessoal/src/main/java/org/Generation/blogPessoal/model/UserLogin.java
+++ blogPessoal/src/main/java/org/Generation/blogPessoal/model/UserLogin.java
@@ -41,10 +41,10 @@ public class UserLogin {
 	public void setToken(String token) {
 		this.token = token;
 	}
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getFoto() {
