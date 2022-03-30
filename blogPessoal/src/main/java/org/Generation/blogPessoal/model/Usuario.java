d21c142ff95b0b76a06159cbba1cf858c5470814
Luiz Felipe Henrique Pereira <lfhenrique95@gmail.com>
2022-03-30 11:41:37 -0300

    add Tipo de usuario

--- blogPessoal/src/main/java/org/Generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/Generation/blogPessoal/model/Usuario.java
@@ -39,6 +39,8 @@ public class Usuario {
 	@NotBlank(message = "O atributo Senha é Obrigatório!")
 	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
+	@Size
+	private String Tipo;
 	
 	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
@@ -104,5 +106,13 @@ public class Usuario {
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
+	}
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.Tipo = tipo;
 	}
 	}
